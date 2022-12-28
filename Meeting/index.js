f2a67ba8958bf4b5acba2d25b50dc34eb5e77918
Tihsrah <harshlf4@gmail.com>
2022-12-28 23:29:06 +0530

    calender updated with animations and bugs fixed for back button

--- Meeting/index.js
+++ Meeting/index.js
@@ -64,3 +64,137 @@ function closeModal(modal) {
   overlay.classList.remove('active')
 }
 
+// calendar actual
+
+let calendar = document.querySelector('.calendar')
+
+const month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+isLeapYear = (year) => {
+    return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 ===0)
+}
+
+getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28
+}
+
+generateCalendar = (month, year) => {
+
+    let calendar_days = calendar.querySelector('.calendar-days')
+    let calendar_header_year = calendar.querySelector('#year')
+
+    let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    calendar_days.innerHTML = ''
+
+    let currDate = new Date()
+    if (!month) month = currDate.getMonth()
+    if (!year) year = currDate.getFullYear()
+
+    let curr_month = `${month_names[month]}`
+    month_picker.innerHTML = curr_month
+    calendar_header_year.innerHTML = year
+
+    // get first day of month
+    
+    let first_day = new Date(year, month, 1)
+
+    for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
+        let day = document.createElement('div')
+        if (i >= first_day.getDay()) {
+            day.classList.add('calendar-day-hover')
+            day.innerHTML = i - first_day.getDay() + 1
+            day.innerHTML += `<span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>`
+            if (i - first_day.getDay() + 1 === currDate.getDate() && year === currDate.getFullYear() && month === currDate.getMonth()) {
+                day.classList.add('curr-date')
+            }
+        }
+        calendar_days.appendChild(day)
+    }
+}
+
+let month_list = calendar.querySelector('.month-list')
+
+month_names.forEach((e, index) => {
+    let month = document.createElement('div')
+    month.innerHTML = `<div data-month="${index}">${e}</div>`
+    month.querySelector('div').onclick = () => {
+        month_list.classList.remove('show')
+        curr_month.value = index
+        generateCalendar(index, curr_year.value)
+    }
+    month_list.appendChild(month)
+})
+
+let month_picker = calendar.querySelector('#month-picker')
+
+month_picker.onclick = () => {
+    month_list.classList.add('show')
+}
+
+let currDate = new Date()
+
+let curr_month = {value: currDate.getMonth()}
+let curr_year = {value: currDate.getFullYear()}
+
+generateCalendar(curr_month.value, curr_year.value)
+
+document.querySelector('#prev-year').onclick = () => {
+    --curr_year.value
+    generateCalendar(curr_month.value, curr_year.value)
+}
+
+document.querySelector('#next-year').onclick = () => {
+    ++curr_year.value
+    generateCalendar(curr_month.value, curr_year.value)
+}
+
+let dark_mode_toggle = document.querySelector('.dark-mode-switch')
+
+dark_mode_toggle.onclick = () => {
+    document.querySelector('body').classList.toggle('light')
+    document.querySelector('body').classList.toggle('dark')
+}
+
+
+// Attendees overlay
+
+// const openModalButtons_attendee = document.querySelectorAll('[data-modal-target-attendees]')
+// const closeModalButtons_attendee = document.querySelectorAll('[data-close-button-attendees]')
+// const overlay_attendee = document.getElementById('overlay-attendees')
+
+// openModalButtons_attendee.forEach(button => {
+//   button.addEventListener('click', () => {
+//     const modal_attendee = document.querySelector(button.dataset.modalTarget)
+//     openModal(modal_attendee)
+//   })
+// })
+
+// overlay.addEventListener('click', () => {
+//   const modals_attendee = document.querySelectorAll('.modal-attendees.active')
+//   modals_attendee.forEach(modal_attendee => {
+//     closeModal(modal_attendee)
+//   })
+// })
+
+// closeModalButtons_attendee.forEach(button => {
+//   button.addEventListener('click', () => {
+//     const modal_attendee = button_attendee.closest('.modal-attendees')
+//     closeModal(modal_attendee)
+//   })
+// })
+
+// function openModal_attendee(modal_attendee) {
+//   if (modal_attendee == null) return
+//   modal_attendee.classList.add('active')
+//   overlay_attendee.classList.add('active')
+// }
+
+// function closeModal_attendee(modal_attendee) {
+//   if (modal_attendee == null) return
+//   modal_attendee.classList.remove('active')
+//   overlay_attendee.classList.remove('active')
+// }
